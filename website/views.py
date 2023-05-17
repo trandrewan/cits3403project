cead07ccd4415f4bfc2882145827500653b3db98
Michael Hardie <23119639@student.uwa.edu.au>
2023-05-18 01:30:11 +0800

    added history page + fixes

--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@
 
 from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
+from . import db
 import openai
 
 views = Blueprint("views", __name__)
